fc59f4485329d281510a8c134f1e1e1af5a44fdc
Jamir <Jamirong06@gmail.com>
2025-02-13 09:46:22 -0500

    added function to btn

--- src/Components/timer/TimerControl.jsx
+++ src/Components/timer/TimerControl.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 const TimerButton = () => {
+    const [time, setTime] = React.useState(0);
+
+    const increment = () => {
+        setTime(time + 1);
+        
+    }
+
     //the component returns a button 
     return(
         <div>
-            <button>Start</button>
+            <button onClick={increment}>Start</button>
+            <h4>Timer has hypothetically started {time} times</h4>
         </div>
     )
 }
